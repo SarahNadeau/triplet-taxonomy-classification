b8c0ebf9736771c10ba09cb127157b7ef87839f1
SarahNadeau <san62@cornell.edu>
2018-02-25 14:17:32 -0500

    works with top k hardest samples

--- Triplet.py
+++ Triplet.py
@@ -1,37 +1,59 @@
+# This script takes in short, raw DNA sequences and performs vector embeddings with a CNN according to an l2
+# triplet loss function.
+    # Currently working on taking n-gram frequencies as input vector instead
+
 import tensorflow as tf
 import numpy as np
 import _pickle as pickle
 from sklearn.neighbors import KNeighborsClassifier
 from tqdm import tqdm
 from tensorflow.python import debug as tf_debug
+import random
 
 
-# load data from standardized train/test set
+# load data from standardized train/test set as raw sequences
 def load_data():
-    with open('/Users/nadeau/Documents/Metagenome_Classification/train_test_set/X_test150.pickle', 'rb') as f:
-        x_test = pickle.load(f)
+    with open('/Users/nadeau/Documents/Metagenome_Classification/Classification_Test_Data/raw_train_test/X_test150.pickle', 'rb') as f:
+        X_test = pickle.load(f)
         f.close()
-    with open('/Users/nadeau/Documents/Metagenome_Classification/train_test_set/X_train150.pickle', 'rb') as f:
-        x_train = pickle.load(f)
+    with open('/Users/nadeau/Documents/Metagenome_Classification/Classification_Test_Data/raw_train_test/X_train150.pickle', 'rb') as f:
+        X_train = pickle.load(f)
         f.close()
-    with open('/Users/nadeau/Documents/Metagenome_Classification/train_test_set/y_test150.pickle', 'rb') as f:
+    with open('/Users/nadeau/Documents/Metagenome_Classification/Classification_Test_Data/raw_train_test/y_test150.pickle', 'rb') as f:
         y_test_str = pickle.load(f)
         y_test = enumerate_y_labels(y_test_str)
         f.close()
-    with open('/Users/nadeau/Documents/Metagenome_Classification/train_test_set/y_train150.pickle', 'rb') as f:
+    with open('/Users/nadeau/Documents/Metagenome_Classification/Classification_Test_Data/raw_train_test/y_train150.pickle', 'rb') as f:
         y_train_str = pickle.load(f)
         y_train = enumerate_y_labels(y_train_str)
         f.close()
-    return x_test, x_train, y_test, y_train
+
+    # to fetch standardized 150 bp train and test vectorized seqs:
+    # X_test = np.genfromtxt(
+    #     '/Users/nadeau/Documents/Metagenome_Classification/mlcircus-lmnn-5b49cafaeb9a/lmnn3/demos/xTe.csv',
+    #     delimiter=',')
+    # X_train = np.genfromtxt(
+    #     '/Users/nadeau/Documents/Metagenome_Classification/mlcircus-lmnn-5b49cafaeb9a/lmnn3/demos/xTr.csv',
+    #     delimiter=',')
+    # y_test = np.genfromtxt(
+    #     '/Users/nadeau/Documents/Metagenome_Classification/mlcircus-lmnn-5b49cafaeb9a/lmnn3/demos/yTe.csv',
+    #     delimiter=',')
+    # y_train = np.genfromtxt(
+    #     '/Users/nadeau/Documents/Metagenome_Classification/mlcircus-lmnn-5b49cafaeb9a/lmnn3/demos/yTr.csv',
+    #     delimiter=',')
+
+    return X_test, X_train, y_test, y_train
 
 # translate string data labels to ints
 def enumerate_y_labels(y_str):
-    ylabel_dict = dict([(y, x) for x, y in enumerate(set(sorted(y_str)))])
+    ylabel_dict = dict([(y, x) for x, y in enumerate(sorted(set(y_str)))])
     return [ylabel_dict[x] for x in y_str]
 
 
 # binarize DNA sequences (takes in sequence)
 def seq2binary(seq):
+    # note: currently train and test samples chosen from genomes so as to not contain "N" bases
+    # count_n = 0
     binary_kmer = np.zeros((len(seq), seq_dim))
     for i in range(0, len(seq)):
         c = seq[i]
@@ -43,23 +65,38 @@ def seq2binary(seq):
             binary_kmer[i, 2] = 1
         elif c == 'G':
             binary_kmer[i, 3] = 1
+        # elif c == 'N':
+        #     binary_kmer[i, 4] = 1
+        #     count_n += 1
+    # if count_n > 0:
+    #     print("{} occurrences of N in sequence".format(count_n))
     return binary_kmer
 
 
 # return lists of samples, positive examples, and negative examples of the specified batch size
 def get_triplet_batch(sample_dict, batch_size):
-    xs = []
-    xps = []
-    xns = []
+    xs = np.zeros(shape=(batch_size, k_mer_len, seq_dim), dtype=np.float64)
+    xps = np.zeros(shape=(batch_size, k_mer_len, seq_dim), dtype=np.float64)
+    xns = np.zeros(shape=(batch_size, k_mer_len, seq_dim), dtype=np.float64)
+
     for i in range(0, batch_size):
         num_classes = len(sample_dict.keys())
-        p_class, n_class = np.random.choice(range(0, num_classes), 2, replace=False)
-        x_ind, p_ind = np.random.choice(range(0, len(sample_dict[p_class])), 2, replace=False)
-        n_ind = np.random.choice(range(0, len(sample_dict[n_class])))
-        xs.append(seq2binary(sample_dict[p_class][x_ind]))
-        xps.append(seq2binary(sample_dict[p_class][p_ind]))
-        xns.append(seq2binary(sample_dict[n_class][n_ind]))
-    return xs, xps, xns
+        p_class, n_class = random.sample(range(0, num_classes), 2)
+
+        x_ind, p_ind = random.sample(range(len(sample_dict[p_class])), 2)
+        n_ind = random.randint(0, len(sample_dict[n_class]) - 1)
+
+        # for raw sequence input
+        xs[i, :] = seq2binary(sample_dict[p_class][x_ind])
+        xps[i, :] = seq2binary(sample_dict[p_class][p_ind])
+        xns[i, :] = seq2binary(sample_dict[n_class][n_ind])
+
+        # for vectorized input
+        # xs.append(sample_dict[p_class][x_ind])
+        # xps.append(sample_dict[p_class][p_ind])
+        # xns.append(sample_dict[n_class][n_ind])
+
+    return np.asarray(xs), np.asarray(xps), np.asarray(xns)
 
 
 # update weights for convolutional filter
@@ -80,14 +117,15 @@ def conv1d(x, W):
 
 
 # compute euclidean distances between two sample embeddings
-def compute_euclidean_distances(x, y, w=None):
+# NOTE: using euclidean distance fails because derivative around zero is vertical, loss goes to NaN
+# instead using l2 norm
+def compute_distances(x, y, w=None):
     x = tf.check_numerics(x, "got passed NaN to compute euclidean distance")
     y = tf.check_numerics(y, "got passed NaN to compute euclidean distance")
     d = tf.subtract(x, y)
     if w is not None:
         d = tf.transpose(tf.multiply(tf.transpose(d), w))
-    # d = tf.check_numerics(tf.sqrt(tf.reduce_sum(d, axis=1)), "euclidean distance wrong")
-    # d = tf.check_numerics(tf.reduce_sum(d, axis=1), "euclidean distance wrong")
+
     return d
 
 
@@ -99,6 +137,11 @@ class Triplet:
 
         # make placeholders for all tensorflow variables
         with tf.variable_scope('input'):
+            # self.x = tf.placeholder(tf.as_dtype(np.float32), shape=[None, kmer_len, seq_dim], name='x')
+            # self.xp = tf.placeholder(tf.as_dtype(np.float32), shape=[None, kmer_len, seq_dim], name='xp')
+            # self.xn = tf.placeholder(tf.as_dtype(np.float32), shape=[None, kmer_len, seq_dim], name='xn')
+
+            # how it was before 2/26
             self.x = tf.placeholder(tf.float32, shape=[None, kmer_len, seq_dim], name='x')
             self.xp = tf.placeholder(tf.float32, shape=[None, kmer_len, seq_dim], name='xp')
             self.xn = tf.placeholder(tf.float32, shape=[None, kmer_len, seq_dim], name='xn')
@@ -109,7 +152,7 @@ class Triplet:
 
             self.pos_weights = tf.placeholder_with_default(shape=[None], name='pos_weights', input=np.array([]))
             self.neg_weights = tf.placeholder_with_default(shape=[None], name='neg_weights', input=np.array([]))
-            self.top_k = tf.placeholder_with_default(shape=[], name='top_k_losses', input=75)
+            self.top_k = tf.placeholder_with_default(shape=[], name='top_k_losses', input=10)
 
         # embed samples
         with tf.variable_scope('embedding') as scope:
@@ -124,20 +167,23 @@ class Triplet:
             self.op = tf.check_numerics(self.op, "op is NaN")
             self.on = tf.check_numerics(self.on, "on is NaN")
 
-            self.dp = compute_euclidean_distances(self.o, self.op)
-            self.dn = compute_euclidean_distances(self.o, self.on)
+            # find o - op and o - on
+            self.dp = compute_distances(self.o, self.op)
+            self.dn = compute_distances(self.o, self.on)
 
-            # check what dimension l2 loss takes norm on
-            tf.Tensor
-            self.dp = tf.nn.l2_loss(self.dp)
-            self.dn = tf.nn.l2_loss(self.dn)
+            # find l2 norm of o - op and o - on
+            self.np = tf.norm(self.dp, axis=1)
+            self.nn = tf.norm(self.dn, axis=1)
+            # self.np = tf.nn.l2_loss(self.dp)
+            # self.nn = tf.nn.l2_loss(self.dn)
 
         # define loss function
         with tf.variable_scope('loss'):
-            self.losses = tf.nn.relu(self.dp - (self.dn - alpha)) + 0.1*self.dn
-            # self.loss = -tf.reduce_mean(tf.nn.top_k(-self.loss, k=self.top_k).values)
-            self.loss = tf.reduce_mean(self.losses)
-            # doesn't help to eliminate top_k
+            # self.losses = tf.nn.relu(self.dp - (self.dn - alpha)) # + 0.1*self.dp
+            self.losses = tf.nn.relu(self.np - (self.nn - alpha))
+            # self.loss = tf.reduce_mean(self.losses)
+            self.top_k_losses = tf.nn.top_k(self.losses, k=self.top_k).values
+            self.loss = tf.reduce_mean(tf.nn.top_k(self.losses, k=self.top_k).values)
 
     # define convolutional network layers
     def __embedding_network(self, x, kmer_len):
@@ -174,31 +220,33 @@ class Triplet:
 
 # SET PARAMETERS HERE
 k_mer_len = 150
-seq_dim = 4
+seq_dim = 4  # 5 if raw sequence input, 1 if vectorized by n-gram frequency input
 embed_dim = 128
 
-batch_size = 32
+batch_size = 64
 margin = 3
-iterations = 100
+iterations = 500
 
 logging_frequency = 10
-top_k_iter_start = 300
-n_easiest = batch_size
+top_k_iter_start = 200
+n_hardest = int(batch_size / 2)
+
+n_neighbors = 3
 
 
 # load data, print summary
-x_test, x_train, y_test, y_train = load_data()
-print("{} training samples".format(len(x_train)))
-print("{} testing samples".format(len(x_test)))
+X_test, X_train, y_test, y_train = load_data()
+print("{} training samples".format(len(X_train)))
+print("{} testing samples".format(len(X_test)))
 
 # build training sample dictionary to group reads by organism
 # keys are integer y_train labels, values are list of corresponding x_train samples
 x_train_dict = {}
-for i in range(0, len(x_train)):
+for i in range(0, len(X_train)):
     if y_train[i] not in x_train_dict:
-        x_train_dict[y_train[i]] = [x_train[i]]
+        x_train_dict[y_train[i]] = [X_train[i]]
     else:
-        x_train_dict[y_train[i]].append(x_train[i])
+        x_train_dict[y_train[i]].append(X_train[i])
 
 # create graph
 triplet = Triplet(kmer_len=k_mer_len, alpha=margin)
@@ -210,33 +258,45 @@ with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
     print("...training  model")
     for i in range(iterations):
-        top_k = batch_size if i < top_k_iter_start else n_easiest
+        top_k = batch_size if i < top_k_iter_start else n_hardest
         batch = get_triplet_batch(x_train_dict, batch_size)
         if i % logging_frequency == 0:
-            loss, losses, dp, dn = sess.run([triplet.loss, triplet.losses, triplet.dp, triplet.dn], feed_dict={triplet.top_k: n_easiest, triplet.x: batch[0],
-                                                     triplet.xp: batch[1], triplet.xn: batch[2]})
+            loss, losses, dp, dn, x, o, top_k_losses, normp, xp, xn = sess.run([triplet.loss, triplet.losses, triplet.dp, triplet.dn, triplet.x, triplet.o, triplet.top_k_losses, triplet.np, triplet.xp, triplet.xn],
+                       feed_dict={triplet.top_k: top_k, triplet.x: batch[0], triplet.xp: batch[1],
+                                  triplet.xn: batch[2]})
+
+            # print("x shape: {}".format(x.shape))
+            # print("xn shape: {}".format(xn.shape))
+            # print("xp shape {}".format(xp.shape))
+            # print("top k: {}".format(top_k))
+            # print("np shape: {}".format(np.shape))
+            # print("top k losses shape: {}".format(top_k_losses.shape))
+            # print("self.o shape: {}".format(o.shape))
+            # print("self.x shape: {}".format(x.shape))
+            # print("self.dp shape: {}".format(dp.shape))
+            # print("self.losses shape: {}".format(losses.shape))
+            # print("self.loss shape: {}".format(loss.shape))
             print('step %d, training loss %g' % (i, loss))
-            print(losses.shape)
         train_step.run(feed_dict={triplet.top_k: top_k, triplet.x: batch[0], triplet.xp: batch[1],
                                   triplet.xn: batch[2]})
 
     # embed training reads -- train_embeddings is a list of 128D sequence embeddings
     print("...classifying")
     train_reads = []
-    for train_read in tqdm(x_train):
+    for train_read in tqdm(X_train):
         train_reads.append(seq2binary(train_read))
     train_embeddings = sess.run(triplet.o, feed_dict={triplet.x: train_reads})
 
     # embed test reads -- test_embeddings is a list of 128D sequence embeddings
     test_reads = []
-    for test_read in tqdm(x_test):
+    for test_read in tqdm(X_test):
         test_reads.append(seq2binary(test_read))
     test_embeddings = sess.run(triplet.o, feed_dict={triplet.x: test_reads})
 
 # find knn for test reads among train reads
-knn_model = KNeighborsClassifier(n_neighbors=3)
+knn_model = KNeighborsClassifier(n_neighbors=n_neighbors)
 knn_model.fit(train_embeddings, y_train)
-print("KNN score: {}".format(knn_model.score(test_embeddings, y_test)))
+print("Testing Error: {}".format((1 - knn_model.score(test_embeddings, y_test))*100))
 
 
 
